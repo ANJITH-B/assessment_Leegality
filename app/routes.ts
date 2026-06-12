4a337b235c3f7c0c4393a7ee8a381ec005c6b11c
ANJITH-B <anjithb200@gmail.com>
2026-06-12 12:23:14 +0530

    chore(config): index route redriect to product

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  // index("routes/home.tsx"),
+  index("routes/home/page.tsx"),
   route("products", "routes/product-list/page.tsx"),
   route("product/:id", "routes/product-detail/page.tsx"),
 ] satisfies RouteConfig;
